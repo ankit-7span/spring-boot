c5f41010f57694710993bf4f14daee8f0be466c3
ankitdabhi <ankit.dabhi.ext@seamless.se>
2022-11-14 14:47:49 +0530

    inital setup

---
+++ src/main/java/com/google/auth/googleauthexample/GoogleAuthExampleApplication.java
@@ -0,0 +1,13 @@
+package com.google.auth.googleauthexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleAuthExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleAuthExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/google/auth/googleauthexample/GoogleAuthExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.auth.googleauthexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleAuthExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
